8b25df5b13425b8777a897e47933487e7bb140a2
dhakidd44 <119883802+dhakidd44@users.noreply.github.com>
2023-11-11 19:39:40 -0400

    Ajout des chemis d adreese de notre base de donnees a l'administrateur

--- Culinary/settings.py
+++ Culinary/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Culinary'
+    'Restaurant'  
 ]
 
 MIDDLEWARE = [
@@ -70,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Culinary.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -80,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -113,7 +111,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- Restaurant/admin.py
+++ Restaurant/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Restaurant, Dish, Order
 
-# Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(Dish)
+admin.site.register(Order)
