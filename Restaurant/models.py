39234307bc9e142e368fdf21865252cfbb9c89ad
dhakidd44 <119883802+dhakidd44@users.noreply.github.com>
2023-11-11 16:02:01 -0400

    Ajout de notre environnement virtuel

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    website = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Dish(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    dishes = models.ManyToManyField(Dish)
+    table_number = models.PositiveIntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id}"
